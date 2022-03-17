ddbc1f484e638cde682a60fbee51ac49c51db20c
My Name <66422260+Radek25@users.noreply.github.com>
2022-03-17 23:18:34 +0100

    Add base system of fetch data from server and add fetchMore system

--- my-app/src/LaunchesPage/LaunchesPage.tsx
+++ my-app/src/LaunchesPage/LaunchesPage.tsx
@@ -4,8 +4,8 @@ import { LaunchesWrapper } from './LaunchesPageStyle';
 import { useState } from 'react';
 
 const GET_LAUNCHES = gql`
-    query GetLaunches($limit: Int!){
-        launchesPast(limit: $limit){
+    query GetLaunches($limit: Int, $offset: Int, $order: String){
+        launchesPast(limit: $limit, offset: $offset, order: $order){
             id
             mission_name
         }
@@ -13,18 +13,21 @@ const GET_LAUNCHES = gql`
 `;
 
 export const LaunchesPage : any  = () => {
-
+    let offset: number = 10;
+    // const [dataArray, setDataArray] = useState[undefined]; - to zastosować!
+    const dataArray: { id: number, mission_name: string }[] = [];
     const [loadDataFlag, setLoadDataFlag] = useState(false);
 
-    const {  data, loading, error } = useQuery(GET_LAUNCHES, {variables: {limit: 9}});
+    const {  data, loading, error, fetchMore } = useQuery(GET_LAUNCHES, {variables: {limit: 10, offset: 0, order: null}});
+    
     if (loading) return "Loading...";
     if (error) return <pre>{error.message}</pre>
-   
+    dataArray.push(...data.launchesPast);
+    
     const handleScroll = (e: React.UIEvent<HTMLUListElement>) => {
        if((e.currentTarget.scrollTop === (e.currentTarget.clientHeight-33)) && loadDataFlag === false){
             setLoadDataFlag(!loadDataFlag);
-            console.log("Ładuje nowe dane")
-            setTimeout(() => {setLoadDataFlag(true); console.log("Koniec ładowania")}, 2000);
+            console.log("Ładuje nowe dane");
        }
     };
 
@@ -32,10 +35,18 @@ export const LaunchesPage : any  = () => {
         <LaunchesWrapper>
             <input type='text' placeholder='Find your racket...'/>
             <ul onScroll={handleScroll}>
-                {data.launchesPast.map((launch: { id: number; mission_name: string; }) => (
-                <li key={launch.id}><span>&#128640;</span>{launch.mission_name}</li>
-                ))}
+                {dataArray.map(launch => <li key={launch.id}>&#128640; {launch.mission_name}</li>)}
             </ul>
+            <button onClick={() => {
+                fetchMore({
+                    variables: {limit: 10, offset: offset},
+                    updateQuery : (prevRes, {fetchMoreResult}) => {
+                        dataArray.push(...fetchMoreResult.launchesPast);
+                    }
+                })
+            }
+            }>Dane</button>
+            <button onClick={() => {console.log(dataArray); offset = offset +10}}>GetArr</button>
         </LaunchesWrapper>
     );
 };
